f780a3dc333ff0a80230eb6509dc231ad3550414
Daniel Bezerra <danielbezerrakmx@gmail.com>
2022-04-14 08:19:53 -0300

    feat: finishing funding main methods and tests

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,121 +1,122 @@
-/*
- * This is an example of a Rust smart contract with two simple, symmetric functions:
- *
- * 1. set_greeting: accepts a greeting, such as "howdy", and records it for the user (account_id)
- *    who sent the request
- * 2. get_greeting: accepts an account_id and returns the greeting saved for it, defaulting to
- *    "Hello"
- *
- * Learn more about writing NEAR smart contracts with Rust:
- * https://github.com/near/near-sdk-rs
- *
- */
 
 // To conserve gas, efficient serialization is achieved through Borsh (http://borsh.io/)
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, setup_alloc};
 use near_sdk::collections::LookupMap;
+use near_sdk::{
+    env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, PromiseOrValue,setup_alloc,
+    serde::{Deserialize, Serialize},
+};
 
 setup_alloc!();
 
-// Structs in Rust are similar to other languages, and may include impl keyword as shown below
-// Note: the names of the structs are not important when calling the smart contract, but the function names are
+#[near_bindgen]
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Funding {
+    funding_value: i8,
+    funding_account_id: String,
+    partial_funding_value: i8
+
+}
+impl Funding {
+    pub fn inc_funding_value(&mut self,amount: i8){
+        self.partial_funding_value += amount;
+    }
+}
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct Welcome {
-    records: LookupMap<String, String>,
+pub struct Contract {
+    funding_memo: LookupMap<String, Funding>
 }
 
-impl Default for Welcome {
-  fn default() -> Self {
-    Self {
-      records: LookupMap::new(b"a".to_vec()),
+impl Default for Contract {
+    fn default() -> Self {
+        Self {
+            funding_memo:LookupMap::new(b"memo".to_vec())
+        }
     }
-  }
 }
-
 #[near_bindgen]
-impl Welcome {
-    pub fn set_greeting(&mut self, message: String) {
-        let account_id = env::signer_account_id();
+impl Contract {
 
-        // Use env::log to record logs permanently to the blockchain!
-        env::log(format!("Saving greeting '{}' for account '{}'", message, account_id,).as_bytes());
+    pub fn create_funding(&mut self, amount:i8 ,account_id: Option<AccountId>){
 
-        self.records.insert(&account_id, &message);
+        let storage_account_id = account_id
+            //convert the valid account ID into an account ID
+            .map(|a| a.into())
+            //if we didn't specify an account ID, we simply use the caller of the function
+            .unwrap_or_else(env::signer_account_id);
+        let funding = Funding { partial_funding_value: 0, funding_value: amount, funding_account_id: storage_account_id.to_string() };
+
+        self.funding_memo.insert(&storage_account_id.to_string(),&funding);
+    }
+    pub fn transfer_money(&mut self, account_id: AccountId,amount:Balance){
+        Promise::new(account_id).transfer(amount);
+
+    }
+    #[payable]
+    pub fn donate_to_funding(&mut self, amount:i8, funding_account_id: AccountId) {
+        let mut record= self.funding_memo.get(&funding_account_id.to_string()).unwrap_or_else(|| env::panic_str("Funding not found."));
+        record.inc_funding_value(amount);
+        self.funding_memo.insert(&funding_account_id.to_string(),&record);
+        if record.partial_funding_value + amount == record.funding_value {
+            self.transfer_money(funding_account_id,near_sdk::env::attached_deposit());
+        }
     }
 
-    // `match` is similar to `switch` in other languages; here we use it to default to "Hello" if
-    // self.records.get(&account_id) is not yet defined.
-    // Learn more: https://doc.rust-lang.org/book/ch06-02-match.html#matching-with-optiont
-    pub fn get_greeting(&self, account_id: String) -> String {
-        match self.records.get(&account_id) {
-            Some(greeting) => greeting,
-            None => "Hello".to_string(),
+    pub fn get_funding(self, user: AccountId) -> Funding {
+        match self.funding_memo.get(&user.to_string()){
+            Some(x)=>x,
+            None => panic!("Didnt find funding"),
         }
     }
 }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- *
- * To run from contract directory:
- * cargo test -- --nocapture
- *
- * From project root, to run in combination with frontend tests:
- * yarn test
- *
- */
-#[cfg(test)]
+
+#[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use super::*;
-    use near_sdk::MockedBlockchain;
+    use near_sdk::test_utils::VMContextBuilder;
     use near_sdk::{testing_env, VMContext};
+    use std::convert::TryInto;
 
-    // mock the context for testing, notice "signer_account_id" that was accessed above from env::
-    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
-        VMContext {
-            current_account_id: "alice_near".to_string(),
-            signer_account_id: "bob_near".to_string(),
-            signer_account_pk: vec![0, 1, 2],
-            predecessor_account_id: "carol_near".to_string(),
-            input,
-            block_index: 0,
-            block_timestamp: 0,
-            account_balance: 0,
-            account_locked_balance: 0,
-            storage_usage: 0,
-            attached_deposit: 0,
-            prepaid_gas: 10u64.pow(18),
-            random_seed: vec![0, 1, 2],
-            is_view,
-            output_data_receivers: vec![],
-            epoch_height: 19,
-        }
+    fn get_context(is_view: bool) -> VMContext {
+        VMContextBuilder::new()
+            .signer_account_id("bob.near".parse().unwrap())
+            .is_view(is_view)
+            .build()
     }
 
     #[test]
-    fn set_then_get_greeting() {
-        let context = get_context(vec![], false);
+    fn test_create_funding() {
+        let key = "bob_near";
+
+        let context = get_context(false);
         testing_env!(context);
-        let mut contract = Welcome::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            "howdy".to_string(),
-            contract.get_greeting("bob_near".to_string())
-        );
+        let mut contract = Contract::default();
+        contract.create_funding(10,Some(key.parse().unwrap()));
+
+        assert_eq!( contract.funding_memo.get(&key.to_string()).unwrap().funding_value,10);
+
     }
 
     #[test]
-    fn get_default_greeting() {
-        let context = get_context(vec![], true);
+    fn test_donate_funding() {
+        let key1 = "bob_near";
+
+        let context = get_context(false);
         testing_env!(context);
-        let contract = Welcome::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            "Hello".to_string(),
-            contract.get_greeting("francis.near".to_string())
-        );
+        let mut contract = Contract::default();
+        contract.create_funding(10,Some(key1.parse().unwrap()));
+        let mut record = contract.funding_memo.get(&key1.to_string()).unwrap_or_else(|| env::panic_str("Record not found."));
+
+        record.inc_funding_value(5);
+        contract.funding_memo.insert(&key1.to_string(), &record);
+
+        assert_eq!( contract.funding_memo.get(&key1.to_string()).unwrap().partial_funding_value,5)
+
     }
+
+
 }
